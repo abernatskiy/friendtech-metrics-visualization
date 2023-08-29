b8f178f2aec236847c0d5f7c4df19d43bf710da9
abernatskiy <abernatskiy@toha.li>
2023-08-30 04:05:46 +0900

    Switching to a local GraphQL source
    	modified:   streams/index.js

--- streams/index.js
+++ streams/index.js
@@ -2,4 +2,4 @@ import * as trades from './trades'
 
 export const streamConfigs = { trades }
 export const title = 'Friend.tech data'
-export const url = 'wss://squid.subsquid.io/friendtech-metrics/v/v1/graphql'
+export const url = 'ws://localhost:4350/graphql'
