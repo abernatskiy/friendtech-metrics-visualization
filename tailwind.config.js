c2fc992e996402e4436213c40eb75ad10b09d8f7
abernatskiy <abernatskiy@toha.li>
2023-09-04 22:16:16 +0900

    Enabling tailwind jit
    	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+	mode: 'jit',
 	content: [
+		'./streams/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	purge: [
+		'./streams/**/*.{js,ts,jsx,tsx}',
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
