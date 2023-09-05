adf72a788f4692bdc073e9b178be6a4a6d53e7b2
abernatskiy <abernatskiy@toha.li>
2023-09-06 00:37:07 +0900

    Negative BigInt support for the eth converter
    	modified:   utils/converters.js

--- utils/converters.js
+++ utils/converters.js
@@ -1,6 +1,8 @@
 export function weiStringToEthAmount(weiStr) {
-	const integerPartDigits = weiStr.length - 18
-	const integerPart = integerPartDigits>0 ? weiStr.substr(0, integerPartDigits) : '0'
-	const fractionalPart = integerPartDigits>0 ? weiStr.substr(integerPartDigits) : '0'.repeat(-integerPartDigits)+weiStr
-	return Number(integerPart + '.' + fractionalPart)
+	const signlessWeiStr = weiStr[0] === '-' ? weiStr.substr(1) : weiStr.substr(0)
+	const integerPartDigits = signlessWeiStr.length - 18
+	const integerPart = integerPartDigits>0 ? signlessWeiStr.substr(0, integerPartDigits) : '0'
+	const fractionalPart = integerPartDigits>0 ? signlessWeiStr.substr(integerPartDigits) : '0'.repeat(-integerPartDigits)+signlessWeiStr
+	const signlessNum = Number(integerPart + '.' + fractionalPart)
+	return weiStr[0] === '-' ? -signlessNum : signlessNum
 }
